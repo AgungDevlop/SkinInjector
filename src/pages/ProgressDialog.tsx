98f078cc6af6621e247fc2a4b6fcaedb745e7d00
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-07-25 12:03:10 +0800

    Update ProgressDialog.tsx

--- src/pages/ProgressDialog.tsx
+++ src/pages/ProgressDialog.tsx
@@ -81,7 +81,7 @@ const ProgressDialog: React.FC<ProgressDialogProps> = ({ progress, setProgress }
                 <p className="error-text">{progress.error}</p>
                 <div className="social-icons">
                   <a
-                    href="https://www.tiktok.com/@agungdev"
+                    href="https://tiktok.com/@neoninject"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="social-icon"
@@ -89,7 +89,7 @@ const ProgressDialog: React.FC<ProgressDialogProps> = ({ progress, setProgress }
                     <FaTiktok />
                   </a>
                   <a
-                    href="https://www.youtube.com/@agungdev"
+                    href="https://youtube.com/@neoninjectofficial?si=AH5n0TtstVo3AMzD"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="social-icon"
@@ -97,7 +97,7 @@ const ProgressDialog: React.FC<ProgressDialogProps> = ({ progress, setProgress }
                     <FaYoutube />
                   </a>
                   <a
-                    href="https://www.instagram.com/@agungdev"
+                    href="https://www.instagram.com/agungeka_22?igsh=dmo4YnR4dTF3cnhq"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="social-icon"
